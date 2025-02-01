0f26e80ae84a0c691e1ef9668bc413ceda4dda73
Emilly <137171497+3mil1y@users.noreply.github.com>
2025-02-01 05:29:44 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-// Defina a chave de acesso da API (se necessário)
-define('API_KEY', '62f37332318e45e6ad11ef1a6af0e42b');
+// Defina a chave de acesso da API
+define('API_KEY', 'SUA_CHAVE_DE_API');
 
 // Endpoint base para a Football-Data.org
 define('API_URL', 'https://api.football-data.org/v4/');
